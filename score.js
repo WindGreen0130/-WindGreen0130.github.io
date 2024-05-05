7394c6c211f87a7947659fcf138595165a512805
WindGreen0130 <154863823+WindGreen0130@users.noreply.github.com>
2024-05-06 07:41:44 +0800

    6

---
+++ score.js
@@ -0,0 +1,14 @@
+var liberalism = 0; /**自由主義 */
+document.getElementById('liberalism').textContent = liberalism;
+
+var socialism = 0; /**社會主義 */
+document.getElementById('socialism').textContent = socialism;
+
+var patriarchy = 0; /**父權 */
+document.getElementById('patriarchy').textContent = patriarchy;
+
+var postmodernism = 0; /**後現代主義 */
+document.getElementById('postmodernism').textContent = postmodernism;
+
+var radicalfeminism = 0; /**基進女權主義 */
+document.getElementById('radicalfeminism').textContent = radicalfeminism;
