d87b0bd5fcc00f25d0cb1231ac3712e55a9d1553
WindGreen0130 <154863823+WindGreen0130@users.noreply.github.com>
2024-05-07 03:31:41 +0800

    3:31

--- score.js
+++ score.js
@@ -1,14 +1,37 @@
-var liberalism = 0; /**自由主義 */
-document.getElementById('liberalism').textContent = liberalism;
+var define = false;
+// var liberalism; /**自由主義 */
+// // document.getElementById('liberalism').textContent = liberalism;
+// var socialism; /**社會主義 */
+// // document.getElementById('socialism').number = socialism;
 
-var socialism = 0; /**社會主義 */
-document.getElementById('socialism').textContent = socialism;
+// var patriarchy; /**父權 */
+// // document.getElementById('patriarchy').textContent = patriarchy;
 
-var patriarchy = 0; /**父權 */
-document.getElementById('patriarchy').textContent = patriarchy;
+// var postmodernism; /**後現代主義 */
+// // document.getElementById('postmodernism').textContent = postmodernism;
 
-var postmodernism = 0; /**後現代主義 */
-document.getElementById('postmodernism').textContent = postmodernism;
+// var radicalfeminism; /**基進女權主義 */
+// // document.getElementById('radicalfeminism').textContent = radicalfeminism;
 
-var radicalfeminism = 0; /**基進女權主義 */
-document.getElementById('radicalfeminism').textContent = radicalfeminism;
+if(define){
+    var liberalism;
+    var socialism;
+    var patriarchy;
+    var postmodernism;
+    var radicalfeminism;
+}
+
+if(liberalism == null) liberalism = 0;
+else liberalism = liberalism;
+
+if(socialism == null) socialism = 0;
+else socialism = socialism;
+
+if(patriarchy == null) patriarchy = 0;
+else patriarchy = patriarchy;
+
+if(postmodernism == null) postmodernism = 0;
+else postmodernism = postmodernism;
+
+if(radicalfeminism == null) radicalfeminism = 0;
+else radicalfeminism = radicalfeminism;
